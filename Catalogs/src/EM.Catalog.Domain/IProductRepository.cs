62713c3a7c3987ba1b36de379e65ae463cadff7f
Willian Hilton Moraes de Jesus <willian.jesus@ltm.digital>
2023-01-10 20:12:40 -0300

    feat: Implementation product repository interface

---
+++ Catalogs/src/EM.Catalog.Domain/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using EM.Catalog.Domain.Entities;
+
+namespace EM.Catalog.Domain;
+
+public interface IProductRepository : IRepository<Product>
+{
+    Task<Product> Add(Product product);
+    Task<Product> Update(Product product);
+    Task<Product> Delete(Product product);
+    Task<Product> GetById(Guid id);
+
+    Task<Category> AddCategory(Category category);
+    Task<Category> UpdateCategory(Category category);
+    Task<Category> DeleteCategory(Guid id);
+    Task<Category> GetCategoryById(Guid id);
+}
---
+++ Catalogs/src/EM.Catalog.Domain/IRepository.cs
@@ -0,0 +1,6 @@
+﻿using EM.Catalog.Domain.Entities;
+
+namespace EM.Catalog.Domain;
+
+public interface IRepository<T> where T : IAggregateRoot
+{ }
