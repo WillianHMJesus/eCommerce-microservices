64f92d29fb603f554fe51be5efd7703cc5349422
Willian Hilton Moraes de Jesus <willian.jesus@ltm.digital>
2023-01-11 14:47:39 -0300

    feat: Implementing add producthandler unit tests

---
+++ Catalogs/tests/EM.Catalog.UnitTests/Application/AddProductHandlerTest.cs
@@ -0,0 +1,21 @@
+﻿using EM.Catalog.Application.Products.Commands.AddProduct;
+using EM.Catalog.Domain;
+using EM.Catalog.Domain.Entities;
+using Moq;
+using Xunit;
+
+namespace EM.Catalog.UnitTests.Application;
+
+public class AddProductHandlerTest
+{
+    [Fact]
+    public async Task Handle_ValidRequest_MustInvokeAddProduct()
+    {
+        Mock<IProductRepository> mockProductRepository = new();
+        AddProductHandler addProductHandler = new(mockProductRepository.Object);
+
+        await addProductHandler.Handle(new AddProductCommand("iPhone 14 Pro", "iPhone 14 Pro 128GB Space Black", 999, 1, "Image iPhone 14 Pro"), CancellationToken.None);
+
+        mockProductRepository.Verify(x => x.Add(It.IsAny<Product>()), Times.Once);
+    }
+}
