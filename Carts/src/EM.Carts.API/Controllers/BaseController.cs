b15e005fbd4d98039bcc1c695ea6ff4ee3b08773
willian Hilton <willian.hilton@hotmail.com>
2023-04-07 11:44:52 -0300

    feat: Implementing the Add Item controller

---
+++ Carts/src/EM.Carts.API/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using EM.Carts.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EM.Carts.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase, IPresenter
+{
+    protected IActionResult _actionResult = null!;
+
+    public void Success(object? data = null)
+        => _actionResult = Ok(data);
+
+    void IBadRequest.BadRequest(object erros)
+        => _actionResult = BadRequest(erros);
+}
---
+++ Carts/src/EM.Carts.API/Controllers/CartsController.cs
@@ -0,0 +1,24 @@
+﻿using EM.Carts.Application.UseCases.AddItem;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EM.Carts.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CartsController : BaseController
+{
+    private readonly IAddItemUseCase _addItemUseCase;
+
+    public CartsController(IAddItemUseCase addItemUseCase)
+        => _addItemUseCase = addItemUseCase;
+
+    [HttpPost("Item")]
+    public async Task<IActionResult> AddItem(AddItemRequest addItemRequest)
+    {
+        _addItemUseCase.SetPresenter(this);
+
+        await _addItemUseCase.ExecuteAsync(addItemRequest);
+
+        return _actionResult;
+    }
+}
