9a9e29ce2b0687ae001fec187553834400d4d6c5
willian Hilton <willian.hilton@hotmail.com>
2023-04-08 15:39:51 -0300

    feat: Deploying the database in the infrastructure

---
+++ Carts/src/EM.Carts.Infraestructure/Configurations/CartDatabaseSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace EM.Carts.Infraestructure.Configurations;
+
+public class CartDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+}
---
+++ Carts/src/EM.Carts.Infraestructure/Configurations/MongoDbConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using EM.Carts.Domain.Entities;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace EM.Carts.Infraestructure.Configurations;
+
+public class MongoDbConfiguration
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbConfiguration(IOptions<CartDatabaseSettings> options)
+    {
+        MongoClient client = new(options.Value.ConnectionString);
+        _database = client.GetDatabase(options.Value.DatabaseName);
+    }
+
+    public IMongoCollection<Cart> CartCollection => 
+        _database.GetCollection<Cart>("Carts");
+}
