52ebb318b23f9ccfa9c711ca45748d9bc7591995
willian Hilton <willian.hilton@hotmail.com>
2024-11-09 19:13:38 -0800

    feat: Inclusion of new error messages

--- Databases/MongoDb/Startup.js
+++ Databases/MongoDb/Startup.js
@@ -15,24 +15,33 @@ db.getCollection('Errors').insertMany([
     { "key": "ProductAnErrorOccorred", "message": "Ocorreu um erro ao cadastrar ou atualizar o produto." },
     { "key": "ProductRegisterDuplicity", "message": "Não é possível cadastrar ou atualizar um produto duplicado." },
     { "key": "ProductNotFound", "message": "Produto não encontrado." },
+    { "key": "ProductUnavailable", "message": "Produto indisponível." },
     { "key": "CategoryCodeLessThanEqualToZero", "message": "O código da categoria não pode ser menor ou igual a zero." },
     { "key": "CategoryNameNullOrEmpty", "message": "O nome da categoria não pode ser vazio ou nulo." },
     { "key": "CategoryDescriptionNullOrEmpty", "message": "A descrição da categoria não pode ser vazio ou nulo." },
     { "key": "CategoryInvalidId", "message": "O id da categoria não pode ser inválido." },
     { "key": "CategoryAnErrorOccorred", "message": "Ocorreu um erro ao cadastrar ou atualizar a categoria." },
     { "key": "CategoryRegisterDuplicity", "message": "Não é possível cadastrar ou atualizar uma categoria duplicada." },
-    { "key": "CategoryNotFound", "message": "Categoria não encontrada." }
+    { "key": "CategoryNotFound", "message": "Categoria não encontrada." },
+    { "key": "CartItemNull", "message": "O Item do carrinho não pode ser nulo." },
+    { "key": "CartNotFound", "message": "Carrinho não encontrado." },
+    { "key": "QuantityGreaterThanAvailable", "message": "A quantidade do item não pode ser maior que a quantidade disponível." },
+    { "key": "ItemNotFound", "message": "Item não encontrado." },
+    { "key": "ValueInvalid", "message": "O valor está inválido." },
+    { "key": "StatusInvalid", "message": "O status está inválido." },
+    { "key": "CardHolderCpfInvalid", "message": "O CPF do cartão está inválido." },
+    { "key": "CardHolderNameInvalid", "message": "O nome do cartão está inválido." },
+    { "key": "CardNumberInvalid", "message": "O número do cartão está inválido." },
+    { "key": "CardExpirationDateInvalid", "message": "A data de expiração do cartão está inválida." },
+    { "key": "CardSecurityCodeInvalid", "message": "O código do cartão está inválido." },
+    { "key": "OrderNumberNull", "message": "O número do pedido não pode ser nulo." },
+    { "key": "OrderItemNull", "message": "O item do pedido não pode ser nulo." },
+    { "key": "OrderIdInvalid", "message": "O id do pedido está inválido." },
+    { "key": "UserIdInvalid", "message": "O Id do usuário está inválido." }
 ]);
 
 db = db.getSiblingDB('Catalog');
 db.createCollection('Categories');
-db.getCollection('Categories').insert({
-    '_id': BinData(3,'f51yszTRpECVu9NCDcUmGw=='),
-    'code': 9999,
-    'name': 'Integration Test',
-    'description': 'Categoria usada nos testes de integração'
-});
-
 db.createCollection('Products');
 
 db = db.getSiblingDB('Cart');
--- Databases/SqlServer/setup.sql
+++ Databases/SqlServer/setup.sql
@@ -7,15 +7,10 @@ create table [Categories](
 	[Code] smallint not null,
 	[Name] varchar(50) not null,
 	[Description] varchar(100) not null,
+	[Active] bit not null,
 
 	constraint [PK_Categories] primary key ([Id]),
 );
-go
-
-DECLARE @ID NVARCHAR(max) = N'b3729d7f-d134-40a4-95bb-d3420dc5261b';
-
-insert into [Categories]
-values(CONVERT(uniqueidentifier, @ID), 9999, 'Integration Test', 'Categoria usada nos testes de integração');
 go
 
 create table [Products](
@@ -26,6 +21,7 @@ create table [Products](
 	[Quantity] smallint not null,
 	[Image] varchar(50) not null,
 	[Available] bit not null,
+	[Active] bit not null,
 	[CategoryId] uniqueidentifier not null,
 
 	constraint [PK_Products] primary key ([Id]),
@@ -40,7 +36,9 @@ use [Checkout];
 create table [Orders](
 	[Id] uniqueidentifier not null,
 	[UserId] uniqueidentifier not null,
+	[Number] varchar(50) not null,
 	[OrderStatus] smallint not null,
+	[Active] bit not null,
 
 	constraint [PK_Orders] primary key ([Id])
 );
@@ -53,9 +51,28 @@ create table [Items](
 	[ProductImage] varchar(50) not null,
 	[Quantity] smallint not null,
 	[Value] decimal(8, 2) not null,
+	[Active] bit not null,
 	[OrderId] uniqueidentifier not null,
 
 	constraint [PK_Items] primary key ([Id]),
 	constraint [FK_Items_Orders] foreign key ([OrderId]) references [Orders] ([Id])
+);
+go
+
+create database [Payment];
+go
+use [Payment];
+
+create table [Transactions](
+	[Id] uniqueidentifier not null,
+	[UserId] uniqueidentifier not null,
+	[OrderId] uniqueidentifier not null,
+	[Value] decimal(8, 2) not null,
+	[CardNumber] varchar(20) not null,
+	[Status] varchar(50) not null,
+	[Date] DateTime not null,
+	[Active] bit not null,
+
+	constraint [PK_Transactions] primary key ([Id])
 );
 go
