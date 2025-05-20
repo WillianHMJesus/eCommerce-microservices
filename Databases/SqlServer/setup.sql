40756c874316ed98226282c82129c70b5fa2eace
unknown <willian.hilton@hotmail.com>
2025-05-20 00:33:42 -0300

    chore: Adding authentication project

--- Databases/SqlServer/setup.sql
+++ Databases/SqlServer/setup.sql
@@ -1,3 +1,86 @@
+create database [Authentication];
+go
+use [Authentication];
+
+create table [Users](
+	[Id] uniqueidentifier not null,
+	[Name] smallint not null,
+	[Email] varchar(50) not null,
+	[Password] varchar(100) not null,
+	[Active] bit not null,
+
+	constraint [PK_Users] primary key ([Id])
+);
+go
+
+create table [Profiles](
+	[Id] uniqueidentifier not null,
+	[Name] smallint not null,
+	[Active] bit not null,
+
+	constraint [PK_Profiles] primary key ([Id])
+);
+go
+
+insert into [Profiles] ([Id], [Name], [Active]) values (newid(), 'Manager', 1);
+go
+
+insert into [Profiles] ([Id], [Name], [Active]) values (newid(), 'Customer', 1);
+go
+
+create table [Roles](
+	[Id] uniqueidentifier not null,
+	[Name] smallint not null,
+	[Active] bit not null,
+
+	constraint [PK_Roles] primary key ([Id])
+);
+go
+
+insert into [Roles] ([Id], [Name], [Active]) values (newid(), 'AddCategory', 1);
+go
+
+insert into [Roles] ([Id], [Name], [Active]) values (newid(), 'DeleteCategory', 1);
+go
+
+insert into [Roles] ([Id], [Name], [Active]) values (newid(), 'UpdateCategory', 1);
+go
+
+insert into [Roles] ([Id], [Name], [Active]) values (newid(), 'AddProduct', 1);
+go
+
+insert into [Roles] ([Id], [Name], [Active]) values (newid(), 'DeleteProduct', 1);
+go
+
+insert into [Roles] ([Id], [Name], [Active]) values (newid(), 'UpdateProduct', 1);
+go
+
+insert into [Roles] ([Id], [Name], [Active]) values (newid(), 'MakeAvailableProduct', 1);
+go
+
+insert into [Roles] ([Id], [Name], [Active]) values (newid(), 'MakeUnavailableProduct', 1);
+go
+
+create table [User_Profiles](
+	[UserId] uniqueidentifier not null,
+	[ProfileId] uniqueidentifier not null,
+
+	constraint [PK_Profiles] primary key ([UserId], [ProfileId]),
+	constraint [FK_User_Profiles_Users] foreign key ([UserId]) references [Users] ([Id]),
+	constraint [FK_User_Profiles_Profiles] foreign key ([ProfileId]) references [Profiles] ([Id])
+);
+go
+
+create table [Profile_Roles](
+	[ProfileId] uniqueidentifier not null,
+	[RoleId] uniqueidentifier not null,
+
+	constraint [PK_Roles] primary key ([ProfileId], [RoleId]),
+	constraint [FK_Profile_Roles_Profiles] foreign key ([ProfileId]) references [Profiles] ([Id]),
+	constraint [FK_Profile_Roles_Roles] foreign key ([RoleId]) references [Roles] ([Id])
+);
+go
+
 create database [Catalog];
 go
 use [Catalog];
@@ -9,7 +92,7 @@ create table [Categories](
 	[Description] varchar(100) not null,
 	[Active] bit not null,
 
-	constraint [PK_Categories] primary key ([Id]),
+	constraint [PK_Categories] primary key ([Id])
 );
 go
 
@@ -25,7 +108,7 @@ create table [Products](
 	[CategoryId] uniqueidentifier not null,
 
 	constraint [PK_Products] primary key ([Id]),
-	constraint [FK_Products_Catalog] foreign key ([CategoryId]) references [Categories] ([Id])
+	constraint [FK_Products_Categories] foreign key ([CategoryId]) references [Categories] ([Id])
 );
 go
 
